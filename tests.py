b17c4ac7c9e2ef1b68204fe2e0de0d93e49ce400
Rinat Akhmetdiyanov <ahmetdianovv@yandex.ru>
2025-01-26 02:16:30 +0300

    fix_final_test_BooksCollector

--- tests.py
+++ tests.py
@@ -21,41 +21,41 @@ class TestBooksCollector:
                                   ["Книга 1", "Книга 2", "Книга 3"])
                              ]
                              )
-    def test_get_books_for_children_two_books(self, books_data, expected_result): # Проверяем фильтрацию книг по жанру, чтобы возвращать только те книги, которые подходят для детей
+    def test_get_books_for_children_two_books(self, books_data, expected_result):
         collector = BooksCollector()
         collector.books_genre = books_data
         assert collector.get_books_for_children() == expected_result
 
 
 
-def test_add_new_book_add_one_books_true (test_books_collector): #Проверяем добавление новой книги
+def test_add_new_book_add_one_books_true (test_books_collector):
     test_books_collector.add_new_book("Книга 1")
     assert "Книга 1" in test_books_collector.get_books_genre()
 
-def test_add_new_book_repeated_name_false(test_books_collector): # Проверяем, что не добавляется книга с повторным названием
+def test_add_new_book_repeated_name_false(test_books_collector):
     test_books_collector.add_new_book("Книга 1")
     assert len(test_books_collector.books_genre) == 1, "Книга не должна добавиться повторно"
 
-def test_add_new_book_long_name_false(test_books_collector):   # Проверяем, что книга с длинным названием не добавляется
+def test_add_new_book_long_name_false(test_books_collector):
     test_books_collector.add_new_book("Книга с длинным названием" + "a" * 40)
     assert len(test_books_collector.books_genre) == 0, "Книга с длинным названием не должна добавляться"
 
 
-def test_set_book_genre_book_collection_fantastic(test_books_collector): # проверяем, корректно ли устанавливается жанр книги
+def test_set_book_genre_book_collection_fantastic(test_books_collector):
     test_books_collector.add_new_book("Книга 1")
     test_books_collector.set_book_genre("Книга 1", "Фантастика")
     assert test_books_collector.get_book_genre("Книга 1") == "Фантастика"
 
-def test_set_book_genre_defunct_book_false(test_books_collector):# Проверяем, что нельзя установить жанр для несуществующей книги
+def test_set_book_genre_defunct_book_false(test_books_collector):
     test_books_collector.set_book_genre("Несуществующая книга", "Фантастика")
     assert test_books_collector.get_book_genre("Несуществующая книга") == None
 
-def test_set_book_genre_defunct_genre_false(test_books_collector): # Проверяем, что нельзя установить несуществующий жанр
+def test_set_book_genre_defunct_genre_false(test_books_collector):
     test_books_collector.set_book_genre("Книга 1", "Несуществующий жанр")
     assert test_books_collector.get_book_genre("Книга 1") != "Фантастика", "Жанр не должен измениться для несуществующего жанра"
 
 
-def test_get_books_with_specific_genre_set_genre_true(test_books_collector):   # проверяем, добавление книг и назначение им жанров, а также может ли корректно возвращать список книг по заданному жанру
+def test_get_books_with_specific_genre_set_genre_true(test_books_collector):
     test_books_collector.add_new_book("Книга 1")
     test_books_collector.set_book_genre("Книга 1", "Фантастика")
 
@@ -69,17 +69,17 @@ def test_get_books_with_specific_genre_set_genre_true(test_books_collector):   #
     assert test_books_collector.get_books_with_specific_genre("Комедии") == ["Книга 3"]
 
 
-def test_add_remove_favorites_cannot_add_again(test_books_collector): # Проверяем, что нельзя добавить книгу повторно в избранное
+def test_add_remove_favorites_cannot_add_again(test_books_collector):
     test_books_collector.add_book_in_favorites("Книга 1")
     assert len(test_books_collector.favorites) == 0, "Книга не должна добавляться повторно в избранное"
 
 
-def test_add_remove_favorites_delete_books(test_books_collector): # Удаляем книгу из избранного
+def test_add_remove_favorites_delete_books(test_books_collector):
     test_books_collector.delete_book_from_favorites("Книга 1")
     assert "Книга 1" not in test_books_collector.get_list_of_favorites_books()
 
 
-def test_get_book_genre_by_name(test_books_collector):  # проверяем получение жанр книги по её имени
+def test_get_book_genre_by_name(test_books_collector):
     test_books_collector.add_new_book("Книга 1")
     test_books_collector.set_book_genre("Книга 1", "Фантастика")
     test_books_collector.add_new_book("Книга 2")
@@ -89,5 +89,5 @@ def test_get_book_genre_by_name(test_books_collector):  # проверяем п�
     assert test_books_collector.get_book_genre("Книга 2") == "Ужасы"
 
 
-def test_get_list_of_favorites_books_true(test_books_with_favorites): # Проверяем получение списка избранных книг
+def test_get_list_of_favorites_books_true(test_books_with_favorites):
     assert set(test_books_with_favorites.get_list_of_favorites_books()) == {"Книга 1", "Книга 2"}
